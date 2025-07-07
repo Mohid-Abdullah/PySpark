24404074fb5afbfb5adabeb9bcd0a4cd40f8c5ad
Mohid Abdullah <141454033+Mohid-Abdullah@users.noreply.github.com>
2025-07-08 01:55:17 +0500

    Create nyc_taxi.py

---
+++ CleanedDataset/nyc_taxi.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum, when
+
+spark = SparkSession.builder.appName("NYC").master("local[*]").getOrCreate()
+
+df = spark.read.parquet(r"CleanDatasets\nyc.parquet", header=True, inferSchema=True)
+df.show(5)
+df.printSchema()
+before = df.count()
+print(before)
+
+df = df.dropna(subset=["passenger_count"])
+df = df.filter((col("passenger_count") > 0) & ((col("passenger_count") < 7)))
+df = df.filter((col("fare_amount") > 0) & (col("trip_distance") > 0))
+
+after = df.count()
+print(before-after)
+
+#checking all coloumns with null value
+nullCols = df.select([sum(when(col(c).isNull(), 1).otherwise(0)).alias(c) for c in df.columns])
+null_Cols_dict = nullCols.collect()[0].asDict()
+
+for col_name, null_count in null_Cols_dict.items():
+    if (null_count == 0):
+        continue
+    else:
+        print(f"{col_name}: {null_count}")
